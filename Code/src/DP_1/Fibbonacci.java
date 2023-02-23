b9bec125de03f520f218a6c09800f6a317beda7f
ritikverma2000 <varmaritik04@gmail.com>
2023-02-23 11:30:24 +0530

    Fibonacci Memorization Solution

---
+++ Code/src/DP_1/Fibbonacci.java
@@ -0,0 +1,31 @@
+public class Fibbonacci {
+
+    //Time Complexity O(n)
+    //Space Complexity O(n) for stack space + O(n) for array
+    public static void main(String[] args) {
+        int n = 5;
+        //Taking dp array of size n+1 
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+
+        int ans = fibo(n, dp);
+        System.out.println(ans);
+    }
+
+    public static int fibo(int n, int[] dp) {
+
+        if (n < 2) {
+            return n;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+
+    }
+
+}
